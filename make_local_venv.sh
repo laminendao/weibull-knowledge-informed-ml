5a6c0d27c8ce1f89bbd75a7848c2276b6c288372
NDAO Mouhamadou-Lamine <mlndao@cesi.fr>
2024-09-18 12:01:07 +0200

    first commit

--- make_local_venv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-conda install -n base -c conda-forge mamba
-mamba env create -f envweibull.yml # install the environment with conda (use mamba)
-eval "$(conda shell.bash hook)"
-conda activate weibull # activate the environment
-pip install -e .
--- src/data/download_data_local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PROJECT_DIR=$1
-
-cd $PROJECT_DIR
-
-python $PROJECT_DIR/src/data/download_data_local.py --path_data_folder $PROJECT_DIR/data
-
